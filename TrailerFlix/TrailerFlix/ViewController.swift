5964f2e0f2d9633c6ccf1fd8de50d9c7dd4c4ddd
Clara Nascimento <clara.nascimento@outlook.com>
2021-02-08 13:12:07 -0300

    124.4 - Add IBOutlet weak var tableview and IBAction func watchRandomTrailer to first scene.

--- TrailerFlix/TrailerFlix/ViewController.swift
+++ TrailerFlix/TrailerFlix/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableview: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func watchRandomTrailer(_ sender: UIButton) {
+    }
+    
 }
 
